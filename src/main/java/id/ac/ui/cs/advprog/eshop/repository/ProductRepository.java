ed0da5f8a6a57d450550a5733e5a19c415c4bb00
fauzanjaisyu <fauzanjaisyu@gmail.com>
2024-02-04 11:11:41 +0700

    update service

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -1,2 +1,36 @@
-package id.ac.ui.cs.advprog.eshop.repository;public class ProductRepository {
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
+@Repository
+public class ProductRepository {
+    private List<Product> productData = new ArrayList<>();
+
+    public Product create(Product product) {
+        productData.add(product);
+        return product;
+    }
+
+    public Iterator<Product> findAll() {
+        return productData.iterator();
+    }
+
+    public Product findById(String productId) {
+        for (Product product : productData) {
+            if (product.getProductId().equals(productId)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public Product edit(Product productName, Product editedProduct){
+        productName.setProductQuantity(editedProduct.getProductQuantity());
+        return productName;
+    }
 }
