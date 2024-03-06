d8c44b9caa1eb1c6803a6295a5a8f947ba267556
fauzanjaisyu <fauzanjaisyu@gmail.com>
2024-03-06 17:10:51 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Map;
+import java.util.Arrays;
+import java.util.List;
 
 @Builder
 @Getter
@@ -13,10 +15,36 @@ public class Payment {
     String method;
     Order order;
     Map<String, String> paymentData;
-    @Setter
     String status;
 
-    public Payment(String id, String method, Order order, Map<String, String> paymentData) {}
+    public Payment(String id, String method, Order order, Map<String, String> paymentData) {
+        this(id, method, order, paymentData, "PENDING");
+    }
+    public Payment(String id, String method, Order order, Map<String, String> paymentData, String status) {
+        this.id = id;
+        this.method = method;
+        this.setOrder(order);
+        this.setPaymentData(paymentData);
+        this.setStatus(status);
+    }
 
-    public Payment(String id, String method, Order order, Map<String, String> paymentData, String status) {}
+    private void setOrder(Order order) {
+        if (order == null) {
+            throw new IllegalArgumentException("Order cannot be null");
+        }
+        this.order = order;
+    }
+
+    protected void setPaymentData(Map<String, String> paymentData) {
+        this.paymentData = null;
+    }
+
+    public void setStatus(String status) {
+        List<String> statusList = Arrays.asList("PENDING", "SUCCESS", "REJECTED");
+
+        if (!statusList.contains(status)) {
+            throw new IllegalArgumentException("Invalid payment status");
+        }
+        this.status = status;
+    }
 }
